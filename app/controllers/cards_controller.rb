478b9533c5622b574b3d7ce1d58822ce48dea821
amcmullen9291 <amcmullen9291@gmail.com>
2021-03-03 20:31:37 -0500

    adding a search form

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -37,18 +37,23 @@ class CardsController < ApplicationController
 
     def index 
         @cards = Card.all
-        @results = Card.search(parmas[:search])
+        @results = Card.search
     end 
 
     private 
     def card_params
-        params.require(:card).permit(:description, :in_stock, :aisle_id, :card, :occasion_id, :sku, :manufacturer_id, :avatar, :search)
+        params.require(:card).permit(:description, :in_stock, :aisle_id, :card, :occasion_id, :sku, :manufacturer_id, :avatar, :sku_search, :occasion_search)
     end
 
     def set_card 
         @card = Card.find(params[:id])
     end 
 
-    def update_notice
+    def self.search
+        if  params[:sku_search]
+            @results = Card.find_by(:sku.includes => params[:sku_search])
+        elsif params[:occsion_search]
+            @results = Card.find_by(:sku.includes => params[:occasion_search])
+        end 
     end 
 end

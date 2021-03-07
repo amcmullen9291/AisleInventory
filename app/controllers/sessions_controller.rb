0a976121c04a8c454e1a78dc394c26b0f554e7d4
amcmullen9291 <amcmullen9291@gmail.com>
2021-03-07 18:00:14 -0500

    double check

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,12 +17,12 @@ class SessionsController < ApplicationController
     if auth = request.env["omniauth.auth"]
       @user = User.find_or_create_by_omniauth(auth)
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to cards_path
     else
       user = User.find_or_create_by(:email => params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect_to root_path
+        redirect_to cards_path
       else
         render 'sessions/new'
       end
